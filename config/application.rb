890d8a1a24d4f9ac4f257347298371031e101f46
tongji53 <tvxq-jyj.01180406@ezweb.ne.jp>
2020-10-27 09:08:55 +0900

    日本語対応化

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module GyakutenCloneGroup
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 日本語化
+    config.i18n.default_locale = :ja
+    # タイムゾーンを東京に設定
+    config.time_zone = "Asia/Tokyo"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
