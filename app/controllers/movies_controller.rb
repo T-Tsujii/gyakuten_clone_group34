97d86f4d645106bde297ae79f2dd18d4989a49a7
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2020-11-18 07:50:11 +0900

    フラッシュをBootstrap使用して実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,24 +4,5 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
-  def create
-    @movie = Movie.new(movie_params)
-    flash[:notice] = "投稿しました。"
-    redirect_to root_path
-  end
-
-  def edit 
-  end
-
-  def update
-    @movie = Movie.update(movie_params)
-    flash[:notice] = "更新しました"
-    redirect_to root_path
-  end
-
-  def destroy
-    @movie = Movie.destroy(movie_params)
-    flash[:notice] = "削除しました"
-    redirect_to root_path
-  end
+  
 end
